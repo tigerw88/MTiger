1887e0aa515beed54cb535235e6cd7892da65c87
jaywellik <jaywellik@vsc-ash.wr.usgs.gov>
2016-05-27 15:02:00 -0700

    ""

--- Finances/defineBudget.m
+++ Finances/defineBudget.m
@@ -1,124 +1,135 @@
-function BudgetCat = defineBudget()
+function [B] = defineBudget()
+% DEFINEBUDGET Define the budget category names, amounts, and corresponding
+% Mint categories
 
-Budget.Type = 'Annual';
+
+
+
 
 %%
 
-BudgetType = 'Monthly Expense';
+mstart = datetime('2015-09-01');
+BudgetDates = mstart + calmonths(1:12);
+
+
+%%
+
 n = 0;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Dining';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 400*12;
-BudgetCat(n).Categories = {'Restaurants', 'Coffee Shops', 'Alcohol & Bars'};
+BudgetCategory{n} = 'Dining';
+BudgetCategoryBudget(n) = -400*12;
+BudgetCategoryCategories{n} = {'Restaurants', 'Coffee Shops', 'Alcohol & Bars'};
+BudgetCategoryExpInc(n) = -1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Living';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 250*12;
-BudgetCat(n).Categories = {'Groceries', 'Home Supplies', 'Pharmacy'};
+BudgetCategory{n} = 'Living';
+BudgetCategoryBudget(n) = -250*12;
+BudgetCategoryCategories{n} = {'Groceries', 'Home Supplies', 'Pharmacy'};
+BudgetCategoryExpInc(n) = -1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Driving';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 70*12;
-BudgetCat(n).Categories = {'Gas & Fuel', 'Parking', 'Public Transportation', 'Public Rail'};
+BudgetCategory{n} = 'Driving';
+BudgetCategoryBudget(n) = -70*12;
+BudgetCategoryCategories{n} = {'Gas & Fuel', 'Parking', 'Public Transportation', 'Public Rail'};
+BudgetCategoryExpInc(n) = -1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Pers. Discretionary';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 250*12;
-BudgetCat(n).Categories = {...
+BudgetCategory{n} = 'Pers. Discretionary';
+BudgetCategoryBudget(n) = -250*12;
+BudgetCategoryCategories{n} = {...
     'Movies & DVDs', 'Books', 'Music', 'Arts', 'Electronics & Software', ...
     'Amusement', 'Park Fees & Camping', 'Sporting Events', ...
     'Education', 'Charity', 'Shipping', 'Misc Items', 'Gift', 'Sports', 'Vacation'};
 % What to do about 'Vacation'?
-
+BudgetCategoryExpInc(n) = -1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Gym';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 100*12;
-BudgetCat(n).Categories = {'Gym'};
+BudgetCategory{n} = 'Gym';
+BudgetCategoryBudget(n) = -100*12;
+BudgetCategoryCategories{n} = {'Gym'};
 % Most of 'Sports' transactions should be changed to 'Gym'
-
+BudgetCategoryExpInc(n) = -1;
 
 
 
 
 %% 
 
-BudgetType = 'Annual Expense';
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Housing';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = (1250*9 + 1000*3);
-BudgetCat(n).Categories = {'Mortgage & Rent', 'Utilities', 'Internet', 'Trash Service', 'Water'};
+BudgetCategory{n} = 'Housing';
+BudgetCategoryBudget(n) = -(1250*9 + 1000*3);
+BudgetCategoryCategories{n} = {'Mortgage & Rent', 'Utilities', 'Internet', 'Trash Service', 'Water'};
+BudgetCategoryExpInc(n) = -1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Shopping';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 3000;
-BudgetCat(n).Categories = {'Shopping', 'Sporting Goods', 'Clothing', 'Bike Service &amp; Parts', 'Home', 'Furnishings'};
+BudgetCategory{n} = 'Shopping';
+BudgetCategoryBudget(n) = -3000;
+BudgetCategoryCategories{n} = {'Shopping', 'Sporting Goods', 'Clothing', 'Bike Service &amp; Parts', 'Home', 'Furnishings'};
+BudgetCategoryExpInc(n) = -1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Auto';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 1500;
-BudgetCat(n).Categories = {'Auto Insurance', 'Service & Parts'};
+BudgetCategory{n} = 'Auto';
+BudgetCategoryBudget(n) = -1500;
+BudgetCategoryCategories{n} = {'Auto Insurance', 'Service & Parts'};
+BudgetCategoryExpInc(n) = -1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Travel';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 250*12;
-BudgetCat(n).Categories = {'Air Travel', 'Rental Car & Taxi', 'Travel'};
+BudgetCategory{n} = 'Travel';
+BudgetCategoryBudget(n) = -250*12;
+BudgetCategoryCategories{n} = {'Air Travel', 'Rental Car & Taxi', 'Travel'};
+BudgetCategoryExpInc(n) = -1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Taxes';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = 60000*0.2;
-BudgetCat(n).Categories = {'State Tax', 'Federal Tax', 'Gov Fine'};
-
+BudgetCategory{n} = 'Taxes';
+BudgetCategoryBudget(n) = -60000*0.2;
+BudgetCategoryCategories{n} = {'State Tax', 'Federal Tax', 'Gov Fine'};
+BudgetCategoryExpInc(n) = -1;
 
 
 %% 
 
-BudgetType = 'Annual Income';
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Salary';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = -60000;
-BudgetCat(n).Categories = {'Paycheck'};
+BudgetCategory{n} = 'Salary';
+BudgetCategoryBudget(n) = 60000;
+BudgetCategoryCategories{n} = {'Paycheck'};
+BudgetCategoryExpInc(n) = 1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Other Income';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = -60000;
-BudgetCat(n).Categories = {'Income', 'Interest Income', 'Gift Received'};
+BudgetCategory{n} = 'Other Income';
+BudgetCategoryBudget(n) = 60000;
+BudgetCategoryCategories{n} = {'Income', 'Interest Income', 'Gift Received'};
+BudgetCategoryExpInc(n) = 1;
 
     %
 n = n+1;
-BudgetCat(n).Name = 'Per Diem';
-BudgetCat(n).BudgetType = BudgetType;
-BudgetCat(n).Amount = -60000;
-BudgetCat(n).Categories = {'Per Diem'};
+BudgetCategory{n} = 'Per Diem';
+BudgetCategoryBudget(n) = 60000;
+BudgetCategoryCategories{n} = {'Per Diem'};
+BudgetCategoryExpInc(n) = 1;
 
+%% Transpose
 
+% make sure everything is sized n-by-1
+B.BudgetDates = BudgetDates;
+B.BudgetCategory = BudgetCategory';
+B.BudgetCategoryBudget = BudgetCategoryBudget';
+B.BudgetCategoryCategories = BudgetCategoryCategories';
+B.BudgetCategoryExpInc = BudgetCategoryExpInc';
 
 %% 
 
