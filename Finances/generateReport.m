32d05c0ec7ac3ce9fa9c4a4649988204042869a4
jaywellik <jaywellik@IGSWWAWGMC163.local>
2016-05-31 21:49:03 -0700

    ""

---
+++ Finances/generateReport.m
@@ -0,0 +1,76 @@
+function [ GroupAmount ] = generateReport( T, B )
+%GENERATEREPORT Summarizes and plots actual spending vs. budget
+
+%   Detailed explanation goes here
+
+%%
+
+display('Generating Report...')
+
+G = B.Group;
+
+for n = 1:numel(G)
+    
+    this_group = G{n};
+    
+    for m = 2:numel(B.BudgetDates)
+    
+        ridx = T.Date >= B.BudgetDates(m-1) & T.Date < B.BudgetDates(m) & strcmp(T.Group, this_group);
+        GroupAmount(n, m) = sum(T{ridx, 'Amount'});
+
+    end
+    
+end
+
+%% Everything
+
+% fh(1) = figure;
+% ax(1) = subplot(1,1,1);
+% barh(sum(GroupAmount(:,:)'))
+% ax(1).YTickLabel = B.Group;
+% 
+% hold on;
+% 
+% plot(abs(B.BudgetAmount),1:numel(G), 'or')
+
+
+%% Expenses
+
+fh(2) = figure;
+idx.exp = 1:10;
+ax(1) = subplot(2,1,1);
+barh(sum(GroupAmount(idx.exp,:)'), 'r');
+% barh((GroupAmount(idx.exp,:)'), 'stacked')
+ax(1).Title.String = 'Expenses';
+ax(1).YTickLabel = B.Group(idx.exp);
+
+hold on;
+
+perc_of_year = (datenum(datevec(max(T.Date))) - datenum(datevec(B.BudgetDates(1))))/(12*30.5);
+plot(abs(B.BudgetAmount(idx.exp)),1:numel(idx.exp), 'ok', 'MarkerFaceColor', 'k');
+plot(abs(B.BudgetAmount(idx.exp))*perc_of_year,1:numel(idx.exp), '*k');
+
+%% Income
+
+idx.inc = 11:13;
+ax(2) = subplot(2,1,2);
+barh(sum(GroupAmount(idx.inc,:)'), 'g');
+ax(2).Title.String = 'Income';
+ax(2).YTickLabel = B.Group(idx.inc);
+
+hold on;
+
+plot(abs(B.BudgetAmount(idx.inc)),1:numel(idx.inc), 'ok', 'MarkerFaceColor', 'k');
+
+%% Ratio
+
+fh(3) = figure;
+labels = {'Savings', 'Expenses'};
+total_income = sum(sum(GroupAmount(idx.inc,:)'));
+total_expenses = sum(sum(GroupAmount(idx.exp,:)'));
+total_savings = total_income - total_expenses;
+pie([total_savings total_expenses]);
+legend(labels,'Location','southoutside','Orientation','horizontal');
+title({'Savings as a % of Income';['Total Income: $' num2str(total_income)]});
+
+end
