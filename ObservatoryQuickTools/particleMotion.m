e906287993a911d64fb355de212fbbe109c4b8d7
jaywellik <jwellik@usgs.gov>
2016-06-02 09:03:23 -0700

    "works with waveform object"

--- ObservatoryQuickTools/particleMotion.m
+++ ObservatoryQuickTools/particleMotion.m
@@ -17,10 +17,10 @@ I could get everything responding the way it is supposed to.
 SEP 22 2015: Add plot labels
 SEP 22 2015: Accept actual seismic data (stub)
 SEP 22 2015: Include sample rate to get indices of seismic data associated with zoom limits
+JUN 02 2016: Works with waveform object
 
 % WISH LIST
 Work with heliPanel and clipboard
-Work with waveform object <-- working on this nows
 Be able to switch to spectogram (similar to working with heliPanel and
 clipboard
 If only a Z channel is provided, search for the existence of N and E channels.
@@ -62,23 +62,25 @@ f = figure;
     % Define space for waveforms & plot
 sp(1) = subplot(2,3, 1:3);
 sp(1).Tag = 'waveform';
-eplot = plot(t,e); hold on; nplot = plot(t,n,'r'); zplot = plot(t,z,'k', 'LineWidth', 2);
-sp(1).XLabel.String = 'Time';
-% sp(1).XTickLabel = {sp(1).XTick/get(w_zne(1), 'freq')};
-% datetick('x','HH:MM:SS');
-% legend([zplot, nplot, eplot], 'Vertical', 'North', 'East');
+% plotting components in reverse order allows Z channel to show up on top
+% order is also relied on during zoom callback
+eplot = plot(w(3), 'r'); hold on
+nplot = plot(w(2), 'b'); hold on
+vplot = plot(w(1), 'k', 'linewidth', 1.25);
+
+legend(w(3:-1:1)) % creating the legend in reverse order makes the colors connect correctly
 
     % Define zoom axes and callbacks
 wp_xlim = 1:length(z);
 zh = zoom(sp(1));
 zh.Motion = 'horizontal';
-zh.ActionPostCallBack = @waveformZoom;
+zh.ActionPostCallBack = {@waveformZoom, w};
 zh.Enable = 'on';
 
     % Define pan and callbacks
 ph = pan(f);
 ph.Motion = 'horizontal';
-ph.ActionPostCallBack = @waveformZoom;
+ph.ActionPostCallBack = {@waveformZoom, w};
 ph.Enable = 'on';
 
     % Define space for E/N particle motion & plot
@@ -116,7 +118,7 @@ end
 
 %% Callback Functions
 
-    function waveformZoom(obj, eventdata)
+    function waveformZoom(obj, eventdata, w)
         
         % obj - figure handle
         %      obj.Children(x) - an axis handle; enumerated in reverse
@@ -126,32 +128,54 @@ end
         %        e.g., obj.Children(4).Children(1) - the handle for the
         %        line for the last waveform data to be plotted
         % eventdata - axis handle for the axis that was clicked on
-        % 
+        %
         % NOTES:
         % waveform data are plotted with the x axis as counts, 1-N
-        % 
+        %
         
-        ndatapts = numel(obj.Children(4).Children(1).XData);
-                
-            % start of new time series window to end of new time series
-            % window - expressed as t1:t2 (???)
-        data_range = floor(eventdata.Axes.XLim(1)):ceil(eventdata.Axes.XLim(2));
+        sr = get(w(1), 'freq'); % get sample rate of waveform object
         
-            % make sure datarange does not exceed data indices
-            % this would happen if the beginning or end click was before
-            % the start of the data or after the end of the data,
-            % respectively
+%         ndatapts = numel(obj.Children(4).Children(1).XData); % number of data points in original waveform trace (in just one trace)
+        ndatapts = numel(get(w(1), 'data')); % number of data points in original waveform trace (in just one trace)
+        
+        
+        %             % start of new time series window to end of new time series
+        %             % window - expressed as t1:t2 (???)
+        %         data_range = floor(eventdata.Axes.XLim(1)):ceil(eventdata.Axes.XLim(2));
+        
+        % start of new time series window to end of new time series
+        % window
+        % XLim values are in seconds, so we multiply by sample rate to
+        % get the data range in samples. Use floor and ceil so that the
+        % data range, which will be used as an index later, is in whole
+        % numbers
+        data_range = floor(eventdata.Axes.XLim(1)*sr):ceil(eventdata.Axes.XLim(2)*sr);
+        
+        % make sure datarange does not exceed data indices
+        % this would happen if the beginning or end click was before
+        % the start of the data or after the end of the data,
+        % respectively
         if data_range(1) < 1, data_range = 1:data_range(end); end;
         if data_range(end) > ndatapts, data_range = data_range(1):ndatapts; end;
         
         
-            % cut data from time series plot
-            % You can't cut from the particle motion plots because they are
-            % cut each time this callback is called, so after one zoom, the
-            % axis might no longer contain all of the data
-        new_e = obj.Children(4).Children(3).YData(data_range);
-        new_n = obj.Children(4).Children(2).YData(data_range);
-        new_z = obj.Children(4).Children(1).YData(data_range);
+        %             % cut data from time series plot
+        %             % You can't cut from the particle motion plots because they are
+        %             % cut each time this callback is called, so after one zoom, the
+        %             % axis might no longer contain all of the data
+        %         new_e = obj.Children(4).Children(3).YData(data_range);
+        %         new_n = obj.Children(4).Children(2).YData(data_range);
+        %         new_z = obj.Children(4).Children(1).YData(data_range);
+        
+        % cut data from original (i.e., complete) waveform data
+        % You can't cut from the particle motion plots because they are
+        % cut each time this callback is called, so after one zoom, the
+        % axis might no longer contain all of the data
+        [d] = get(w, 'data'); % grabs trace data as 3-by-1 cell array where each cell is an n-by-1 vector of data points
+        new_z = d{1}(data_range);
+        new_n = d{2}(data_range);
+        new_e = d{3}(data_range);
+        
         
             % reset the particle motion data
             % (each one described as'Y/X plot')
